c21d90c4035aab96003c5f7e7cbfc97c8ed2b270
Julia Johnson <julialj95@gmail.com>
2021-04-26 18:27:47 -0500

    changed api_base_url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_BASE_URL:
-    process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
+    "https://vast-cove-98655.herokuapp.com/api" || "http://localhost:8000/api",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
 
